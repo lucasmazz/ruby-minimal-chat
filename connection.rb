2b97290a79dac0296be54fdf63d7c83ed8d6c095
Lucas Mazzetto <lucasrmazzetto@gmail.com>
2014-12-26 00:55:04 -0200

    Bump

--- connection.rb
+++ connection.rb
@@ -4,54 +4,54 @@ require 'socket'
 
 module Connection
 
-	class Connection
-		def initialize
+    class Connection
+        def initialize
             Thread.new { loop { puts @conn.recv 1024 } }
             loop { @conn.puts(gets) }
-		end
+        end
 
-		def send(text)
-			@conn.puts text
-		end
+        def send(text)
+            @conn.puts text
+        end
 
-		def close
-			@conn.close
-			exit
-		end
-	end
+        def close
+            @conn.close
+            exit
+        end
+    end
 
 
-	class Server < Connection
+    class Server < Connection
 
-		def initialize(ip="127.0.0.1", port=5555)
-			@ip = ip
-			@port = port
+        def initialize(ip="127.0.0.1", port=5555)
+            @ip = ip
+            @port = port
 
-			server = TCPServer.open(@ip, @port)
+            server = TCPServer.open(@ip, @port)
             puts "Server is listening on #{@ip}:#{@port}, waiting for connection."
             @conn = server.accept
             puts "A connection has been established."
             puts "---------------------------------"
 
-			super()
-		end
-	end
+            super()
+        end
+    end
 
 
-	class Client < Connection
+    class Client < Connection
 
-		def initialize(ip="127.0.0.1", port=5555)
-			@ip = ip
-			@port = port
+        def initialize(ip="127.0.0.1", port=5555)
+            @ip = ip
+            @port = port
 
-			@conn = TCPSocket.new(@ip, @port)
+            @conn = TCPSocket.new(@ip, @port)
             puts "Successfully connected."
             puts "---------------------------------"
 
-			super()
-		end
+            super()
+        end
 
-	end
+    end
 
 end
 
--- main.rb
+++ main.rb
@@ -5,45 +5,45 @@ require './connection'
 
 if __FILE__ == $0
 
-	options = {}
+    options = {}
 
-	OptionParser.new do |opts|
-		opts.banner = "Usage: example.rb [options]"
+    OptionParser.new do |opts|
+        opts.banner = "Usage: example.rb [options]"
 
-		opts.on('-i', '--name NAME', 'Source Ip') do |v|
-			block = /[01]\d{,2}|2[0-4]\d|25[0-5]/
+        opts.on('-i', '--name NAME', 'Source Ip') do |v|
+            block = /[01]\d{,2}|2[0-4]\d|25[0-5]/
 
-			if v =~ /\A#{block}\.#{block}\.#{block}\.#{block}\z/
-				options[:ip] = v
-			else
-				raise "The -i parameter must be an ip"
-			end
-		end
+            if v =~ /\A#{block}\.#{block}\.#{block}\.#{block}\z/
+                options[:ip] = v
+            else
+                raise "The -i parameter must be an ip"
+            end
+        end
 
-		opts.on('-p', '--port PORT', 'Source Port') do |v|
-			if v =~ /^\d+$/
-				options[:port] = v
-			else
-				raise "The -p parameter must be a number"
-			end
-		end
+        opts.on('-p', '--port PORT', 'Source Port') do |v|
+            if v =~ /^\d+$/
+                options[:port] = v
+            else
+                raise "The -p parameter must be a number"
+            end
+        end
 
-		opts.on('-m', '--mod MOD', 'Source Mod') do |v|
-			if v.downcase =~ /^client$/ or v.downcase =~ /^server$/
-				options[:mod] = v.downcase
-			else
-				raise 'The -m parameter must be "client" or "server"'
-			end
-		end
+        opts.on('-m', '--mod MOD', 'Source Mod') do |v|
+            if v.downcase =~ /^client$/ or v.downcase =~ /^server$/
+                options[:mod] = v.downcase
+            else
+                raise 'The -m parameter must be "client" or "server"'
+            end
+        end
 
-	end.parse!
+    end.parse!
 
 
-	# verifies the type of connection
-	if options[:mod] == "server"
-		conn = Connection::Server.new(options[:ip], options[:port].to_i)
-	elsif options[:mod] == "client"
-		conn = Connection::Client.new(options[:ip], options[:port].to_i)
-	end
+    # verifies the type of connection
+    if options[:mod] == "server"
+        conn = Connection::Server.new(options[:ip], options[:port].to_i)
+    elsif options[:mod] == "client"
+        conn = Connection::Client.new(options[:ip], options[:port].to_i)
+    end
 
 end
